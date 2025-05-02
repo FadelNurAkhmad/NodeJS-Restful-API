406604be42fdb2a063d7c352eb2993ddfb95294f
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-02 07:15:46 +0700

    Register API User

--- src/application/web.js
+++ src/application/web.js
@@ -1,4 +1,12 @@
 import express from "express";
+import { publicRouter } from "../route/public-api.js";
+import { errorMiddleware } from "../middleware/error-middleware.js";
 
 export const web = express();
+
+// menggunakan middleware
 web.use(express.json());
+
+web.use(publicRouter);
+
+web.use(errorMiddleware);
