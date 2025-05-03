488b7161750784b7564a2b7aacb0668e156c8e37
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-03 18:05:51 +0700

    Login User API

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -3,7 +3,7 @@ import { ResponseError } from "../error/response-error.js";
 const validate = (schema, request) => {
   const result = schema.validate(request, {
     abortEarly: false,
-    allowUnknown: false,
+    allowUnknown: false, // memvalidasi apabila ada field asing yg masuk
   });
   if (result.error) {
     throw new ResponseError(400, result.error.message);
