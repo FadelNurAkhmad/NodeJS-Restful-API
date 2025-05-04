2c3995e0e0ef3ad4dde5b85e54634bdea1c71112
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-04 09:27:53 +0700

    Get User API

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -2,6 +2,7 @@ import { validate } from "../validation/validation.js";
 import {
   loginUserValidation,
   registerUserValidation,
+  getUserValidation,
 } from "../validation/user-validation.js";
 import { prismaClient } from "../application/database.js";
 import { ResponseError } from "../error/response-error.js";
@@ -72,7 +73,28 @@ const login = async (request) => {
   });
 };
 
+const get = async (username) => {
+  username = validate(getUserValidation, username);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+
+  if (!user) {
+    throw new ResponseError(404, "user is not found");
+  }
+
+  return user;
+};
+
 export default {
   register,
   login,
+  get,
 };
