df287896857224dfa2672ae2efff8211cc4c3761
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-11 11:21:25 +0700

    Get, Update Address API

--- src/controller/address-controller.js
+++ src/controller/address-controller.js
@@ -16,6 +16,43 @@ const create = async (req, res, next) => {
   }
 };
 
+const get = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const contactId = req.params.contactId;
+    const addressId = req.params.addressId;
+
+    const result = await addressService.get(user, contactId, addressId);
+
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const contactId = req.params.contactId;
+    const addressId = req.params.addressId;
+    const request = req.body;
+    request.id = addressId;
+    // Kita menambahkan properti baru (id) langsung ke objek yang sudah ada tanpa harus membuat objek baru.
+
+    const result = await addressService.update(user, contactId, request);
+
+    res.status(200).json({
+      data: result,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 export default {
   create,
+  get,
+  update,
 };
